86e14912bf078b00d09723770bde32de6c6e6d19
Alexey Groshev <aleksejs.grosevs@gmail.com>
2014-05-18 17:18:37 +0100

    Added crawling for releases and deployments.

--- octo.py
+++ octo.py
@@ -27,14 +27,14 @@ class UrlFactory:
     def url_environment(self, env_id='all'):
         return self.url_api() + '/environments/all' if env_id == 'all' else self.url_api() + '/environments/' + env_id
 
+    def url_project(self, proj_id='all'):
+        return self.url_api() + '/projects/all' if proj_id == 'all' else self.url_api() + '/projects/' + proj_id
+
     def url_deployments(self):
         return self.url_api() + '/deployments'
 
     def url_release(self, rel_id='all'):
         return self.url_api() + '/releases' if rel_id == 'all' else self.url_api() + '/releases/' + rel_id
-
-    def url_project(self, proj_id='all'):
-        return self.url_api() + '/projects' if proj_id == 'all' else self.url_api() + '/projects/' + proj_id
 
 
 class Octopy:
@@ -61,46 +61,52 @@ class Octopy:
     def get_projects(self, cache=False):
         if cache:
             return self.__read_objects(self.file_projects)
-        self.projects = Octopy.__extract_objects(self.__scrape(self.url_factory.url_project())['Items'], 'Id', 'Name')
+        self.projects = Octopy.__extract_objects(self.__scrape(self.url_factory.url_project()), 'Id', 'Name')
         self.__save_objects(self.file_projects, self.projects)
         return self.projects
 
-    def get_releases(self, cache=False):
+    def get_releases(self, cache=False, crawl=False):
         if cache:
             return self.__read_objects(self.file_releases)
-        self.releases = Octopy.__extract_objects(self.__scrape(self.url_factory.url_release())['Items'], 'Id', 'Version')
+
+        url = self.url_factory.url_release()
+        while url:
+            response = self.__scrape(url)
+            self.releases.update(Octopy.__extract_objects(response['Items'], 'Id', 'Version'))
+            url = self.__get_next_url(crawl, response)
+
         self.__save_objects(self.file_releases, self.releases)
         return self.releases
 
-    def get_deployments(self, cache=False):
+    def get_deployments(self, cache=False, crawl=False):
         if cache:
             return self.__read_list(self.file_deployments)
 
         self.get_environments(cache=False)
         self.get_projects(cache=False)
-        self.get_releases(cache=False)
+        self.get_releases(False, crawl)
         self.deployments = []
-        response = self.__scrape(self.url_factory.url_deployments())
 
-        for dep in response['Items']:
-            if dep['ReleaseId'] not in self.releases:
-                release = self.__scrape(self.url_factory.url_release(dep['ReleaseId']))
-                self.releases[release['Id']] = release['Version']
+        url = self.url_factory.url_deployments()
+        while url:
+            response = self.__scrape(url)
+            for dep in response['Items']:
+                if not crawl:
+                    if dep['ReleaseId'] not in self.releases:
+                        release = self.__scrape(self.url_factory.url_release(dep['ReleaseId']))
+                        self.releases[release['Id']] = release['Version']
 
-            if dep['ProjectId'] not in self.projects:
-                project = self.__scrape(self.url_factory.url_project(dep['ProjectId']))
-                self.projects[project['Id']] = project['Name']
+                dt = dateutil.parser.parse(dep['Created'])
 
-            dt = dateutil.parser.parse(dep['Created'])
-
-            self.deployments.append({
-                'Id': dep['Id'],
-                'Date': dt.date(),
-                'Time': dt.time().strftime('%H:%M'),
-                'Environment': self.environments[dep['EnvironmentId']],
-                'Project': self.projects[dep['ProjectId']],
-                'Release': self.releases[dep['ReleaseId']]
-            })
+                self.deployments.append({
+                    'Id': dep['Id'],
+                    'Date': dt.date(),
+                    'Time': dt.time().strftime('%H:%M'),
+                    'Environment': self.environments[dep['EnvironmentId']],
+                    'Project': self.projects[dep['ProjectId']],
+                    'Release': self.releases[dep['ReleaseId']]
+                })
+            url = self.__get_next_url(crawl, response)
 
         self.__save_objects(self.file_projects, self.projects)
         self.__save_objects(self.file_releases, self.releases)
@@ -150,6 +156,12 @@ class Octopy:
                 result.append(row)
         return result
 
+    def __get_next_url(self, crawl, json):
+        if crawl:
+            return self.config['server'] + json['Links']['Page.Next'] if json['Links'] and 'Page.Next' in json['Links'] else None
+        else:
+            return None
+
 
 def main():
     config = get_configs('octopy.cfg')
@@ -163,6 +175,10 @@ def main():
     parser.add_argument('--cmd', dest='command', help="Octopy command (try `env`, `proj`, `rel` and `dep`).")
     parser.add_argument('--cache', dest='cache', action='store_true', help="Read data from cache if available.")
     parser.add_argument('--headers', dest='headers', action='store_true', help='Display headers in output.')
+    parser.add_argument('--crawl', dest='crawl', action='store_true',
+        help='By default only 30 items per page are returned by API. This parameter enables link crawl. '
+             'All resources from the `Link` collection will be crawled by Octopy and data will be saved to cache. '
+             'This parameter has no effect on `env` and `proj` commands.')
     args = parser.parse_args()
 
     octopy = Octopy(config)
@@ -180,13 +196,13 @@ def main():
         for key in projects.keys():
             print '%s,%s' % (key, projects[key])
     elif args.command == 'rel':  # releases
-        releases = octopy.get_releases(args.cache)
+        releases = octopy.get_releases(args.cache, args.crawl)
         if args.headers:
             print 'Id,Version'
         for key in releases.keys():
             print '%s,%s' % (key, releases[key])
     elif args.command == 'dep':  # deployments
-        deployments = octopy.get_deployments(args.cache)
+        deployments = octopy.get_deployments(args.cache, args.crawl)
         if args.headers:
             print 'Date,Time,Environment,Project,Release'
         for dep in deployments:
