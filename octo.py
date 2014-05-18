1eeb173abae5cdd38529e7de2fc8ae43511ef624
Alexey Groshev <aleksejs.grosevs@gmail.com>
2014-05-18 14:42:02 +0100

    Added commands for projects and releases.

--- octo.py
+++ octo.py
@@ -21,14 +21,6 @@ class UrlFactory:
     def __init__(self, server):
         self.server = server
 
-    def get_url(self, command):
-        if command == 'env':
-            return self.url_environment(), 1
-        elif command == 'dep':
-            return self.url_deployments(), 2
-        else:
-            return None, 0
-
     def url_api(self):
         return self.server + '/api'
 
@@ -45,52 +37,118 @@ class UrlFactory:
         return self.url_api() + '/projects' if proj_id == 'all' else self.url_api() + '/projects/' + proj_id
 
 
-def scrape(url, api_key):
-    return requests.get(url, headers={'X-Octopus-ApiKey': api_key}).json()
+class Octopy:
+    def __init__(self, config):
+        self.environments = {}
+        self.projects = {}
+        self.releases = {}
+        self.deployments = []
+        self.config = config
+        self.url_factory = UrlFactory(self.config['server'])
+        # cache file names
+        self.file_environments = 'environments.csv'
+        self.file_projects = 'projects.csv'
+        self.file_releases = 'releases.csv'
+        self.file_deployments = 'deployments.csv'
 
+    def get_environments(self, cache=False):
+        if cache:
+            return self.__read_objects(self.file_environments)
+        self.environments = Octopy.__extract_objects(self.__scrape(self.url_factory.url_environment()), 'Id', 'Name')
+        self.__save_objects(self.file_environments, self.environments)
+        return self.environments
 
-def extract_objects(json, o_id, o_value):
-    result = {}
-    for obj in json:
-        result[obj[o_id]] = obj[o_value]
-    return result
+    def get_projects(self, cache=False):
+        if cache:
+            return self.__read_objects(self.file_projects)
+        self.projects = Octopy.__extract_objects(self.__scrape(self.url_factory.url_project())['Items'], 'Id', 'Name')
+        self.__save_objects(self.file_projects, self.projects)
+        return self.projects
 
+    def get_releases(self, cache=False):
+        if cache:
+            return self.__read_objects(self.file_releases)
+        self.releases = Octopy.__extract_objects(self.__scrape(self.url_factory.url_release())['Items'], 'Id', 'Version')
+        self.__save_objects(self.file_releases, self.releases)
+        return self.releases
 
-def save_objects(dir_name, file_name, objects):
-    if not os.path.isdir(dir_name):
-        os.makedirs(dir_name)
-    with open('%s/%s' % (dir_name, file_name), 'w') as f:
-        w = csv.writer(f, delimiter=',', quotechar='|', lineterminator='\n')
-        for k in objects.keys():
-            w.writerow([k, objects[k]])
+    def get_deployments(self, cache=False):
+        if cache:
+            return self.__read_list(self.file_deployments)
 
+        self.get_environments(cache=False)
+        self.get_projects(cache=False)
+        self.get_releases(cache=False)
+        self.deployments = []
+        response = self.__scrape(self.url_factory.url_deployments())
 
-def read_objects(dir_name, file_name):
-    result = {}
-    with open('%s/%s' % (dir_name, file_name), 'r') as f:
-        reader = csv.reader(f, delimiter=',', quotechar='|', lineterminator='\n')
-        for row in reader:
-            result[row[0]] = row[1]
-    return result
+        for dep in response['Items']:
+            if dep['ReleaseId'] not in self.releases:
+                release = self.__scrape(self.url_factory.url_release(dep['ReleaseId']))
+                self.releases[release['Id']] = release['Version']
 
+            if dep['ProjectId'] not in self.projects:
+                project = self.__scrape(self.url_factory.url_project(dep['ProjectId']))
+                self.projects[project['Id']] = project['Name']
 
-def save_list(dir_name, file_name, list):
-    if not os.path.isdir(dir_name):
-        os.makedirs(dir_name)
-    keys = ['Id', 'Date', 'Time', 'Environment', 'Project', 'Release']
-    with open('%s/%s' % (dir_name, file_name), 'w') as f:
-        w = csv.DictWriter(f, keys, delimiter=',', quotechar='|', lineterminator='\n')
-        w.writerows(list)
+            dt = dateutil.parser.parse(dep['Created'])
 
+            self.deployments.append({
+                'Id': dep['Id'],
+                'Date': dt.date(),
+                'Time': dt.time().strftime('%H:%M'),
+                'Environment': self.environments[dep['EnvironmentId']],
+                'Project': self.projects[dep['ProjectId']],
+                'Release': self.releases[dep['ReleaseId']]
+            })
 
-def read_list(dir_name, file_name):
-    result = []
-    keys = ['Id', 'Date', 'Time', 'Environment', 'Project', 'Release']
-    with open('%s/%s' % (dir_name, file_name), 'r') as f:
-        reader = csv.DictReader(f, keys, delimiter=',', quotechar='|', lineterminator='\n')
-        for row in reader:
-            result.append(row)
-    return result
+        self.__save_objects(self.file_projects, self.projects)
+        self.__save_objects(self.file_releases, self.releases)
+        self.__save_list(self.file_deployments, self.deployments)
+        return self.deployments
+
+    def __scrape(self, url):
+        return requests.get(url, headers={'X-Octopus-ApiKey': self.config['api_key']}).json()
+
+    @staticmethod
+    def __extract_objects(json, o_id, o_value):
+        result = {}
+        for obj in json:
+            result[obj[o_id]] = obj[o_value]
+        return result
+
+    def __save_objects(self, file_name, objects):
+        if not os.path.isdir(self.config['dir_tmp']):
+            os.makedirs(self.config['dir_tmp'])
+        with open('%s/%s' % (self.config['dir_tmp'], file_name), 'w') as f:
+            w = csv.writer(f, delimiter=',', quotechar='|', lineterminator='\n')
+            for k in objects.keys():
+                w.writerow([k, objects[k]])
+
+    def __read_objects(self, file_name):
+        result = {}
+        with open('%s/%s' % (self.config['dir_tmp'], file_name), 'r') as f:
+            reader = csv.reader(f, delimiter=',', quotechar='|', lineterminator='\n')
+            for row in reader:
+                result[row[0]] = row[1]
+        return result
+
+    def __save_list(self, file_name, list):
+        if not os.path.isdir(self.config['dir_tmp']):
+            os.makedirs(self.config['dir_tmp'])
+        keys = ['Id', 'Date', 'Time', 'Environment', 'Project', 'Release']
+        with open('%s/%s' % (self.config['dir_tmp'], file_name), 'w') as f:
+            w = csv.DictWriter(f, keys, delimiter=',', quotechar='|', lineterminator='\n')
+            w.writerows(list)
+
+    def __read_list(self, file_name):
+        result = []
+        keys = ['Id', 'Date', 'Time', 'Environment', 'Project', 'Release']
+        with open('%s/%s' % (self.config['dir_tmp'], file_name), 'r') as f:
+            reader = csv.DictReader(f, keys, delimiter=',', quotechar='|', lineterminator='\n')
+            for row in reader:
+                result.append(row)
+        return result
 
 
 def main():
@@ -100,80 +158,42 @@ def main():
         print 'Please, specify Octopus parameters in configuration file!'
         sys.exit(1)
 
-    parser = argparse.ArgumentParser(description='OctoPy is a small application that prints out information from Octopus in a convenient format.')
-    parser.add_argument('--cmd', dest='command', help="Octopy command (try `env` and `dep`).")
+    parser = argparse.ArgumentParser(
+        description='Octopy is a small application that prints out information from Octopus in a convenient format.')
+    parser.add_argument('--cmd', dest='command', help="Octopy command (try `env`, `proj`, `rel` and `dep`).")
     parser.add_argument('--cache', dest='cache', action='store_true', help="Read data from cache if available.")
-    parser.add_argument('--headings', dest='headings', action='store_true', help='Display headings in output.')
+    parser.add_argument('--headers', dest='headers', action='store_true', help='Display headers in output.')
     args = parser.parse_args()
 
-    if args.command is None:
-        print "Please specify command"
-        parser.print_help()
-        sys.exit(1)
+    octopy = Octopy(config)
 
-    urlFactory = UrlFactory(config['server'])
-    url, command_type = urlFactory.get_url(args.command)
-
-    if command_type == 0:
-        print "Unknown command '%s'" % args.command
-        sys.exit(1)
-
-    if args.cache:
-        environments = read_objects(config['dir_tmp'], 'environments.csv')
-    else:
-        environments = extract_objects(scrape(urlFactory.url_environment(), config['api_key']), 'Id', 'Name')
-        for env in environments.keys():
-            environments[env] = environments[env]
-        save_objects(config['dir_tmp'], 'environments.csv', environments)
-
-    if command_type == 1:
-        if args.headings:
+    if args.command == 'env':  # environments
+        environments = octopy.get_environments(args.cache)
+        if args.headers:
             print 'Id,Name'
-        for env in environments.keys():
-            print '%s,%s' % (env, environments[env])
-    elif command_type == 2:
-        if args.headings:
+        for key in environments.keys():
+            print '%s,%s' % (key, environments[key])
+    elif args.command == 'proj':  # projects
+        projects = octopy.get_projects(args.cache)
+        if args.headers:
+            print 'Id,Name'
+        for key in projects.keys():
+            print '%s,%s' % (key, projects[key])
+    elif args.command == 'rel':  # releases
+        releases = octopy.get_releases(args.cache)
+        if args.headers:
+            print 'Id,Version'
+        for key in releases.keys():
+            print '%s,%s' % (key, releases[key])
+    elif args.command == 'dep':  # deployments
+        deployments = octopy.get_deployments(args.cache)
+        if args.headers:
             print 'Date,Time,Environment,Project,Release'
-
-        if args.cache:
-            deployments = read_list(config['dir_tmp'], 'deployments.csv')
-            for deployment in deployments:
-                print '%s,%s,%s,%s,%s' % \
-                      (deployment['Date'], deployment['Time'], deployment['Environment'], deployment['Project'], deployment['Release'])
-            sys.exit(0)
-
-        projects = extract_objects(scrape(urlFactory.url_project(), config['api_key'])['Items'], 'Id', 'Name')
-        releases = extract_objects(scrape(urlFactory.url_release(), config['api_key'])['Items'], 'Id', 'Version')
-
-        response = scrape(url, config['api_key'])
-        deployments = []
-
-        for dep in response['Items']:
-            dt = dateutil.parser.parse(dep['Created'])
-
-            if dep['ReleaseId'] not in releases:
-                rel = scrape(urlFactory.url_release(dep['ReleaseId']), config['api_key'])
-                releases[rel['Id']] = rel['Version']
-
-            if dep['ProjectId'] not in projects:
-                proj = scrape(urlFactory.url_project(dep['ProjectId']), config['api_key'])
-                projects[proj['Id']] = proj['Name']
-
-            deployment = {
-                'Id': dep['Id'],
-                'Date': dt.date(),
-                'Time': dt.time().strftime('%H:%M'),
-                'Environment': environments[dep['EnvironmentId']],
-                'Project': projects[dep['ProjectId']],
-                'Release': releases[dep['ReleaseId']]
-            }
-            deployments.append(deployment)
-            print '%s,%s,%s,%s,%s' %\
-                  (deployment['Date'], deployment['Time'], deployment['Environment'], deployment['Project'], deployment['Release'])
-
-        save_objects(config['dir_tmp'], 'projects.csv', projects)
-        save_objects(config['dir_tmp'], 'releases.csv', releases)
-        save_list(config['dir_tmp'], 'deployments.csv', deployments)
+        for dep in deployments:
+            print '%s,%s,%s,%s,%s' % (dep['Date'], dep['Time'], dep['Environment'], dep['Project'], dep['Release'])
+    else:
+        print "Unknown command '%s'" % args.command
+        parser.print_help()
 
 
 if __name__ == '__main__':
